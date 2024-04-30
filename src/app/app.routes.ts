7ab932d66f16341c4b483ae0b82fd56c68dcb9b3
Ogunlade Adebayo Peter <144332360+Adebayozz1994@users.noreply.github.com>
2024-04-30 11:34:36 -0100

    the beginning

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SignupComponent } from './signup/signup.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path:'navbar', component: NavbarComponent},
+    {path:'signup', component: SignupComponent},
+
+];
