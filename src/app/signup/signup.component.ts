a890072cb9df08e4a1d270a5278348a4a3554f34
Ogunlade Adebayo Peter <144332360+Adebayozz1994@users.noreply.github.com>
2024-05-01 16:58:48 -0100

    new commit

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,13 +1,50 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+
+interface ContactInterface{
+  firstname:string,
+  lastname:string,
+  email:string,
+  password:string
+  address: string 
+}
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [FormsModule, CommonModule,ReactiveFormsModule, HttpClientModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
-export class SignupComponent {
 
+export class SignupComponent {
+  constructor(public Router:Router,public formBuilder:FormBuilder){}
+  message:any=''
+  public contact :ContactInterface ={
+    firstname:'',
+    lastname:'',
+    email:'',
+   password:'',
+    address:''
+  
+ }
+
+ contactarray:any=[]
+ submit() {
+
+   const UserDetails = {
+     name: this.contact.firstname,
+     last_name: this.contact.lastname,
+     email: this.contact.email,
+     password: this.contact.password,
+     address: this.contact.address
+   }
+   console.log(UserDetails);
+   
+  }
 }
