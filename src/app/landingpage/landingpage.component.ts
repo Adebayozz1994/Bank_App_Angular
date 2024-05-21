67f476bc98f72c5a43f3ef368e4a08a843b817e3
Ogunlade Adebayo Peter <144332360+Adebayozz1994@users.noreply.github.com>
2024-05-21 12:31:04 -0100

    new commit

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MaterialmoduleModule } from '../materialmodule/materialmodule.module';
 import { Router} from '@angular/router';
 import { FormsModule } from '@angular/forms';
@@ -10,12 +10,20 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.css'
 })
-export class LandingpageComponent {
+export class LandingpageComponent implements OnInit{
+  isLoading: boolean = true;
   constructor(public rout:Router){}
+  
 signup(){
   this.rout.navigate([`/signup`]);
 }
 login(){
   this.rout.navigate(['/login'])
+}
+ngOnInit(): void {
+  
+  setTimeout(() => {
+    this.isLoading = false;
+  }, 3000); 
 }
 }
